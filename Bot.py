0195d16b32987230ae56d32420414cab8c50941c
Adrien Carrou <dridriswim@gmail.com>
2022-04-07 14:47:52 -0700

    linux service

--- Bot.py
+++ Bot.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.service import Service
 import time
 
 from telegram.ext.updater import Updater
