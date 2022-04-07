273b33cea54c2ecaf71cdf74187b2c2b4d911f3d
Adrien Carrou <dridriswim@gmail.com>
2022-04-07 13:46:57 -0700

    working script with telegram bot

--- Bot.py
+++ Bot.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
 
 from telegram.ext.updater import Updater
 from telegram.update import Update
@@ -15,18 +17,18 @@ from telegram.ext.filters import Filters
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 
-campground_link = "https://www.recreation.gov/camping/campgrounds/232755"
+campground_link = "https://www.recreation.gov/camping/campgrounds/233116"
 #campground_link = "https://www.recreation.gov/camping/campgrounds/233116"
 ChromeWebDriverPath = r"C:\chromedriver_win32\chromedriver.exe"
 
-camp_month = "Jun"
-camp_day = "5"
-number_of_days = 5
-group_amount = 4
-camp_site_num = "006"
-    #"APA09"
-#001-033 for Big Sur campground
+#----Important----
+camp_month = "Oct" #Three letter month example: Apr, Jun, Mar...
+camp_day = "21"
+number_of_days = 3
 
+#----Less Important----
+group_amount = 2
+camp_site_num = "022"
 email_login = "adriencarrou@gmail.com"
 password_login = "ForTesting12345"
 postal_code = "94025"
@@ -46,23 +48,27 @@ driver.get(campground_link)
 # wait for the page to load
 button = driver.find_element(by=By.CLASS_NAME, value="sarsa-button.sarsa-modal-close-button.sarsa-button-subtle.sarsa-button-md")
 button.click()
-# find the first link on the page
 
-#IN PROGRESS
-# wait for the page to load
-first_day = number_of_days
-while number_of_days>0 and int(camp_day)<=31:
-    button = driver.find_element(by=By.XPATH, value=f"//button[@aria-label='{camp_month} {camp_day}, 2022 - Site {camp_site_num} is available']")
-    driver.execute_script("arguments[0].click();", button)
+#WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//button[@aria-label='{camp_month} {camp_day}, 2022 - Site {camp_site_num} is available']")))
 
-    camp_day = int(camp_day)
-    if number_of_days == first_day:
-        camp_day+=2
-        number_of_days -=2
-    camp_day+=1
-    number_of_days-=1
-    camp_day = str(camp_day)
-driver.execute_script("window.scrollTo(0, 1000)")
+while True:
+    try:
+        first_day = number_of_days
+        while number_of_days > 0 and int(camp_day) <= 31:
+            button = driver.find_element(by=By.XPATH,
+                                         value=f"//button[@aria-label='{camp_month} {camp_day}, 2022 - Site {camp_site_num} is available']")
+            driver.execute_script("arguments[0].click();", button)
+            camp_day = int(camp_day)
+            if number_of_days == first_day:
+                camp_day += 2
+                number_of_days -= 2
+            camp_day += 1
+            number_of_days -= 1
+            camp_day = str(camp_day)
+        driver.execute_script("window.scrollTo(0, 1000)")
+        break
+    except NoSuchElementException:
+        continue
 
 '''
 add_to_cart = WebDriverWait(driver, 10).until(
@@ -82,15 +88,9 @@ password.send_keys(password_login)
 SignIn = driver.find_element(by=By.XPATH, value="/html/body/div[4]/div/div/div/div[2]/div/div/div[2]/form/button")
 SignIn.click()
 
-#------This should be uncommented if the campground does not have accessibility flag
-'''driver.implicitly_wait(3)
-AccessButton = driver.find_element(by=By.XPATH, value="//*[@id='accessibility-modal-alert']/div/div/div/div/div/div/div/button[2]")
-AccessButton.click()'''
-
 driver.implicitly_wait(5)
 postal = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[2]/section[1]/div[2]/div[2]/div[3]/div[3]/div/div[2]/div[1]/input")
 postal.send_keys(postal_code)
-
 
 driver.implicitly_wait(3)
 group_size = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[2]/section[1]/div[2]/div[3]/div/div/div/div/div[2]/button[2]")
@@ -109,63 +109,9 @@ accept_terms.click()
 updater = Updater("5273245540:AAEUT573S0buicRc2876h5O9_uF30JDmQko",
                   use_context=True)
 
+#telegram bot
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "I am the camping bot here to help you when there is an available spot for you in your specified camp ground. I will notify you when your spot is available!")
-
     update.message.reply_text(
         f"Hello {first_name}! There is an available spot camping spot for you, and you have 15 minutes to fully reserve your spot!!! {driver.current_url}")
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.start_polling()
-
-#/html/body/div[14]/div/div/div/div[2]/button[1]
-
-'''driver.implicitly_wait(10)
-boat_equipment = driver.find_element(by=By.CLASS_NAME, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[2]/section[1]/div[3]/div[4]/div/div[1]/div/div/label/span[1]")
-boat_equipment.click()
-
-driver.implicitly_wait(5)
-boat_length = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[2]/section[1]/div[3]/div[4]/div/div/div/div/div/div[2]/div[1]/input")
-boat_length.send_keys(10)'''
-
-'''driver.implicitly_wait(5)
-go_to_cart = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[3]/div/div/div/div[2]/button")
-go_to_cart.click()
-
-driver.implicitly_wait(5)
-go_to_payment = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div/div[1]/div[2]/div/div/div[3]/button")
-go_to_payment.click()
-
-
-driver.implicitly_wait(5)
-payment_first_name = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[1]/div[1]/input")
-payment_first_name.send_keys(credit_first_name)
-
-driver.implicitly_wait(5)
-payment_last_name = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[1]/div[2]/input")
-payment_last_name.send_keys(credit_last_name)
-
-driver.implicitly_wait(5)
-cc_num = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[2]/input")
-cc_num.send_keys(credit_card_num)
-
-driver.implicitly_wait(5)
-exp_month = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[3]/div[1]/select")
-exp_month.is_selected(cc_exp_month)
-
-driver.implicitly_wait(5)
-exp_year = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[3]/div[2]/select")
-exp_year.is_selected(cc_exp_year)
-
-
-driver.implicitly_wait(5)
-security_cvv = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[4]/div/input")
-security_cvv.send_keys(credit_card_num)
-
-driver.implicitly_wait(5)
-next_pay = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/button")
-next_pay.send_keys(credit_card_num)'''
-
-
-
-
---
+++ payment_script.py
@@ -0,0 +1,51 @@
+
+
+
+
+#/html/body/div[14]/div/div/div/div[2]/button[1]
+
+'''driver.implicitly_wait(10)
+boat_equipment = driver.find_element(by=By.CLASS_NAME, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[2]/section[1]/div[3]/div[4]/div/div[1]/div/div/label/span[1]")
+boat_equipment.click()
+
+driver.implicitly_wait(5)
+boat_length = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[2]/section[1]/div[3]/div[4]/div/div/div/div/div/div[2]/div[1]/input")
+boat_length.send_keys(10)'''
+
+'''driver.implicitly_wait(5)
+go_to_cart = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/section/div/div[1]/div[3]/div/div/div/div[2]/button")
+go_to_cart.click()
+
+driver.implicitly_wait(5)
+go_to_payment = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div/div[1]/div[2]/div/div/div[3]/button")
+go_to_payment.click()
+
+
+driver.implicitly_wait(5)
+payment_first_name = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[1]/div[1]/input")
+payment_first_name.send_keys(credit_first_name)
+
+driver.implicitly_wait(5)
+payment_last_name = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[1]/div[2]/input")
+payment_last_name.send_keys(credit_last_name)
+
+driver.implicitly_wait(5)
+cc_num = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[2]/input")
+cc_num.send_keys(credit_card_num)
+
+driver.implicitly_wait(5)
+exp_month = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[3]/div[1]/select")
+exp_month.is_selected(cc_exp_month)
+
+driver.implicitly_wait(5)
+exp_year = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[3]/div[2]/select")
+exp_year.is_selected(cc_exp_year)
+
+
+driver.implicitly_wait(5)
+security_cvv = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/div[4]/div/input")
+security_cvv.send_keys(credit_card_num)
+
+driver.implicitly_wait(5)
+next_pay = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[4]/div/div/div/div[1]/div[2]/div[1]/div/div[2]/button")
+next_pay.send_keys(credit_card_num)'''
